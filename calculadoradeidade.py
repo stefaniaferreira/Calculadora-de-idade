f4621afb4191dcd750ab4c59b6f9f4b560e284b0
Stefania Ferreira <sstefaniaferreira@gmail.com>
2026-07-12 22:33:19 -0300

    Add files via upload

---
+++ calculadoradeidade.py
@@ -0,0 +1,11 @@
+#Perguntar o nome da pessoa, o ano de nascimento e calcular a idade da pessoa
+
+nome = input("Qual é o seu nome?")
+ano_nascimento = int(input("Qual é o seu ano de nascimento?"))
+idade = 2026 - ano_nascimento
+
+print("Meu nome é: ", nome)
+print("Sua idade é de : ", idade, "anos")
+
+
+
