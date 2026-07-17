88d22ed9680a5198a5a955b78bf68142b709b7ce
Stefania Ferreira <sstefaniaferreira@gmail.com>
2026-07-17 12:58:53 -0300

    Implement input validation for year of birth
    Added input validation for birth year to ensure numeric input.

--- calculadoradeidade.py
+++ calculadoradeidade.py
@@ -1,13 +1,25 @@
-#Perguntar o nome da pessoa, o ano de nascimento e calcular a idade da pessoa
+#Perguntar o nome da pessoa, o ano de nascimento (aaaa) e calcular a idade da pessoa
 
 from datetime import date
 nome = input("Qual é o seu nome? ")
-ano_nascimento = int(input("Qual é o seu ano de nascimento? "))
 
-#Criar uma função para calcular a idade da pessoas
+while True:
+    try:
+        ano_nascimento = int(input("Qual é o seu ano de nascimento? "))
+        break
+    except ValueError:
+        print("Por favor, digite apenas números.")
+
+#Formatar o ano em 4 dígitos e criar uma função para calcular a idade da pessoa 
+
 def calcular_idade(ano_nascimento):
     ano_atual = date.today().year
     idade = ano_atual - ano_nascimento
     return idade
 
 print(f"{nome}, você tem {calcular_idade(ano_nascimento)} anos.")
+
+
+
+
+
